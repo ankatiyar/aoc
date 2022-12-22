6376d781e8a5e626661bcaf9fee8ea105c4eca9d
Ankita Katiyar <ankitakatiyar2401@gmail.com>
2022-12-22 17:08:18 +0000

    day 5 and 6

---
+++ solutions/solution_06.py
@@ -0,0 +1,12 @@
+with open('../inputs/input06.txt') as f:
+    lines = f.read()
+
+
+def parse_message(lines, n):
+    for i in range(len(lines) - n):
+        if len(set(lines[i:i + n])) == n:
+            return i + n
+
+
+print("start of datastream : ", parse_message(lines, 4))
+print("start of message : ", parse_message(lines, 14))
